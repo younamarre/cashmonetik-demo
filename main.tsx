f206b33d898e89ca20c74cad1c82e6e8069f1ebc
younamarre <youssef@fomow.co>
2025-05-02 16:51:15 +0200

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => (
+  <div style={{ color: "#fff", textAlign: "center", marginTop: "3rem" }}>
+    <h1>Bienvenue sur CashMonétik</h1>
+    <p>Page en cours de développement.</p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
